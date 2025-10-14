58f9bbc13294b3a903bac340507fc9c5058cf018
intodotspace <admin@into.space>
2025-10-14 14:21:22 -0400

    Documentation edits made through Mintlify web editor

--- snippets/leverage-impact-chart.jsx
+++ snippets/leverage-impact-chart.jsx
@@ -256,7 +256,7 @@ export const LeverageImpactChart = () => {
           
           {/* Entry price vertical line at 15% */}
           <line x1="152" y1="40" x2="152" y2="320" stroke="currentColor" strokeWidth="2" strokeDasharray="8,4" opacity="0.6">
-            <animate attributeName="stroke-dashoffset" values="12;0;12" dur="1.5s" repeatCount="indefinite"/>
+            <animate attributeName="stroke-dashoffset" values="0;12" dur="1.5s" repeatCount="indefinite"/>
           </line>
           
           {/* Leverage lines with clipping - REVERSED ORDER */}
